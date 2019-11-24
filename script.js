211f0dc7251d115ba0fea3c946b9631b104f4620
Lucas Assis <lga.lima@hotmail.com>
2019-11-24 19:44:56 -0300

    mensagem default no editor de codigo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.getElementById("content").innerHTML =
-localStorage["text"] || "This text is automatically saved every second :) "; // default text
+localStorage["text"] || "Digite seu codigo"; // default text
 setInterval(function() {
   // fuction that is saving the innerHTML of the div
   localStorage["text"] = document.getElementById("content").innerHTML; // content div
